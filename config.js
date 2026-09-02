41a1efa65f91c15c2e5188ce062fec5452a6b25e
ChidoriYoshida <hikochan.farm@gmail.com>
2026-09-02 14:20:57 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Apps Script の「ウェブアプリ」デプロイ後に発行されるURLをここに設定してください。
 // 例: "https://script.google.com/macros/s/AKfycb.../exec"
-const API_URL = "https://script.google.com/macros/s/AKfycbz-2XsYf3vsBljJaSR_knQrdCLlt1Ysn576ujNLe2KaC0OTXsb8aGTO3gEb9TUQ2iSnAA/exec";
+const API_URL = "ここにApps Script の Web App URL を貼り付け";
